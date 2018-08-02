6ce922d5d733d6bb76ba4e34c73a1f97d1a3f89f
venky <venkycse93@gmail.com>
2018-08-02 21:47:14 +0530

    Reverted sciencemarket view to laravel framework view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-<div id="bg-window"></div>
-<div id="main-container">
-    <br>
-    <div id="login-logo">
-        <img id="" src="{{asset('images/logo4.svg')}}" width="70" height="70"/>
-        <img id="" src="{{asset('images/logo.svg')}}" width="250" height="70"/>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <br>
-    <form method="post" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-        <div class="form-group form-section">
-            <label for="name-area" class="form-labels">{{ __('Username') }}</label>
-            <input class="form-control input-class" id="name-area" type="text" name="userid" placeholder="Enter login ID" value="{{ old('userid') }}" />
-        </div>
-        <div class="form-group form-section">
-            <label for="pwd-area" class="form-labels">{{ __('Password') }}</label>
-            <input class="form-control input-class" id="pwd-area" type="password" name="pwd" placeholder="Enter password" />
-        </div>
-        
-    <!-- <div class="g-signin2" data-onsuccess="onSignIn"></div></br> -->
-        
-        <div class="form-row-2">
-            <table>
-            <tr><td>
-                <span class="checkbox">
-                    <label><input class="" type="checkbox" value="" name="remMe">Remember me</label>
-                </span>
-                </td>
-                <td>
-                <a id="fp-link" href="frgt_pwd.php" target="_blank" name="fgtPwd">Forgot Password?</a>
-                </td>
-            </tr>
-            </table>
-        </div></br>
-        <div class="form-row-3">
-            <input class="btn btn-primary inp-button" type="submit" value="Sign in" />
-            <a href="register.php" class="btn btn-primary inp-button">New user? Sign up</a>
-        </div>
-    </form>
 </div>
 @endsection
